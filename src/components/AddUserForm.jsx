6172ed9370c9478147d6bf9a62aad88444191e5b
Edgar <EdgarGuce14@gmail.com>
2021-10-12 20:34:31 -0500

    UseStyle test

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -3,8 +3,21 @@ import { useForm } from "react-hook-form";
 //import Button from "./ui/Button";
 import Label from "./ui/Label";
 
-import { Button, Icon } from "@material-ui/core/";
-import DeleteIcon from "@material-ui/icons/Delete";
+import { Button, Icon, Typography } from "@material-ui/core/";
+import { makeStyles } from "@material-ui/core/styles";
+//import DeleteIcon from "@material-ui/icons/Delete";
+
+const useStyle = makeStyles({
+  myClassName: {
+    background: "#444444",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5 px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+});
 
 const AddUserForm = (props) => {
   const { register, errors, handleSubmit } = useForm();
@@ -16,6 +29,8 @@ const AddUserForm = (props) => {
     //clear fields
     e.target.reset();
   };
+
+  const classes = useStyle();
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -45,6 +60,10 @@ const AddUserForm = (props) => {
       >
         Add New User
       </Button>
+      <Typography variant="h4" color="primary" align="center">
+        Hola Mundo
+      </Typography>
+      <Button className={classes.myClassName}>Boton</Button>
     </form>
   );
 };
--- src/components/ui/Label.jsx
+++ src/components/ui/Label.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Typography } from "@material-ui/core";
+
 const Label = (props) => {
-  return <label>{props.text}</label>;
+  return (
+    <Typography variant="h5" color="primary">
+      {props.text}
+    </Typography>
+  );
 };
 
 export default Label;
